5691009b938d2457db55a3de5af21e0020ba47e5
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-09-01 18:32:47 -0500

    SS - Connect Gateway to OrdersMicroservices

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, ParseIntPipe } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { ORDER_SERVICE } from 'src/config';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(@Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy) { }
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersClient.send('findAllOrders', {});
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersClient.send('findOneOrder', { id });
+  }
+
+}
