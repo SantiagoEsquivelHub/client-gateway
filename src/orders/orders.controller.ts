540bc4a10656bf087659f9045df816d76379e4aa
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-09-15 10:47:25 -0500

    SS - RpcCustomExceptionFilter

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -16,8 +16,15 @@ export class OrdersController {
   }
 
   @Get()
-  findAll(@Query() paginationDto: OrderPaginationDto) {
-    return this.client.send('findAllOrders', paginationDto);
+  async findAll(@Query() paginationDto: OrderPaginationDto) {
+    try {
+      const orders = await firstValueFrom(
+        this.client.send('findAllOrders', paginationDto)
+      );
+      return orders;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
   @Get('/id/:id')
